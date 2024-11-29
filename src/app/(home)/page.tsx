bdc2037b1fc42f16a63032cda449b57013f9b0d9
addadadsdeefef <adam.tesak99@gmail.com>
2024-11-29 09:46:47 +0100

    test1

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,3 +14,5 @@ export default async function HomePage() {
   // Conditionally render authenticated or non-authenticated home view
   return session ? <AuthHomeView session={session} /> : <NonAuthHomeView />;
 }
+
+
